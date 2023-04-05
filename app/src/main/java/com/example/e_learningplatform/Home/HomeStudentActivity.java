95d48103f30834be0c7a2f443a6c3ac0442ef2c1
Weasel0507 <adi.redex2@yahoo.com>
2023-04-05 23:57:51 +0300

    Login Page design done.

---
+++ app/src/main/java/com/example/e_learningplatform/Home/HomeStudentActivity.java
@@ -0,0 +1,19 @@
+package com.example.e_learningplatform.Home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.e_learningplatform.R;
+
+public class HomeStudentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_student);
+
+
+    }
+}
--- app/src/main/java/com/example/e_learningplatform/LoginActivity.java
+++ app/src/main/java/com/example/e_learningplatform/LoginActivity.java
@@ -2,13 +2,31 @@ package com.example.e_learningplatform;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.e_learningplatform.Home.HomeStudentActivity;
 
 public class LoginActivity extends AppCompatActivity {
-
+Button sing_in_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sing_in_btn = findViewById(R.id.sing_in_button);
+
+        sing_in_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(getApplicationContext(), HomeStudentActivity.class);
+                startActivity(i);
+
+            }
+        });
+
     }
 }
